ca4b894422b2c56df06435f8ca7885da130adaed
Bobo <bnbsking@gmail.com>
2022-05-14 13:56:20 +0800

    Update kinetics.py

--- kinetics.py
+++ kinetics.py
@@ -488,10 +488,10 @@ class VideoMAE(torch.utils.data.Dataset):
         images = self._video_TSN_decord_batch_loader(directory, decord_vr, duration, segment_indices, skip_offsets)
         # List[PIL] # len=16 # PIL.shape=(320,240)
 
-        process_data, mask = self.transform((images, None)) # T*C,H,W # [48,224,224], [1,0,...] len=1668
+        process_data, mask = self.transform((images, None)) # T*C,H,W # [48,224,224], [1,0,...] len=1568
         process_data = process_data.view((self.new_length, 3) + process_data.size()[-2:]).transpose(0,1)  # T*C,H,W -> T,C,H,W -> C,T,H,W
         
-        return (process_data, mask)
+        return (process_data, mask) # shape=(3,16,224,224) # shape=(1568,)
 
     def __len__(self):
         return len(self.clips)
