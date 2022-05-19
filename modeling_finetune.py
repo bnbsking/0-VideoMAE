add3b6ef60c6a1167a6c54290f14b26b35596017
Bobo <bnbsking@gmail.com>
2022-05-19 17:22:51 +0800

    Update modeling_finetune.py

--- modeling_finetune.py
+++ modeling_finetune.py
@@ -102,7 +102,7 @@ class Block(nn.Module):
 
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., init_values=None, act_layer=nn.GELU, norm_layer=nn.LayerNorm,
-                 attn_head_dim=None):
+                 attn_head_dim=None): # 384, 12, 4, True, None, 0, 0, 0, nn.LayerNorm, None
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.attn = Attention(
