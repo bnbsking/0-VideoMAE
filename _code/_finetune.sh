f84ef5406c74999214b5c1c02910c3ffab5faf56
Bobo <bnbsking@gmail.com>
2022-08-23 10:00:47 +0800

    Add files via upload

---
+++ _code/_finetune.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+DATA_PATH='/home/jovyan/data-vol-1/VideoMAE/_data/downstream_0810_10_v2' # labeled
+#DATA_PATH='/home/jovyan/data-vol-1/VideoMAE/_data/pretext_0810_10' # unlabeled
+
+OUTPUT_DIR='/home/jovyan/data-vol-1/VideoMAE/_exps/downstream_0810_10_v2'
+
+MODEL_PATH='/home/jovyan/data-vol-1/VideoMAE/_exps/pretext_0810_10_lr2/checkpoint-3199.pth' # finetune
+#MODEL_PATH='/home/jovyan/data-vol-1/VideoMAE/_exps/downstream_0810_10_lr4/best/checkpoint-best.pth' # eval
+
+python run_class_finetuning.py \
+    --model vit_base_patch16_224 \
+    --data_set UCF101 \
+    --nb_classes 4 \
+    --data_path ${DATA_PATH} \
+    --finetune ${MODEL_PATH} \
+    --log_dir ${OUTPUT_DIR} \
+    --output_dir ${OUTPUT_DIR} \
+    --batch_size 8 \
+    --num_sample 1 \
+    --input_size 224 \
+    --short_side_size 224 \
+    --save_ckpt_freq 10 \
+    --num_frames 16 \
+    --sampling_rate 4 \
+    --opt adamw \
+    --lr 1e-3 \
+    --opt_betas 0.9 0.999 \
+    --weight_decay 0.05 \
+    --epochs 2000 \
+    --dist_eval \
+    --test_num_segment 5 \
+    --test_num_crop 3 \
+    --mixup 0 \
+    --cutmix 0 \
+    --smoothing 0 \
+    #--eval
+    #--enable_deepspeed
---
+++ _code/_multieval.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+DATA_PATH='/home/jovyan/data-vol-1/VideoMAE/_data/downstream_0810_10'
+OUTPUT_DIR='/home/jovyan/data-vol-1/VideoMAE/_exps/downstream_0810_10_lr4'
+
+for FINETUNE_WEIGHT in $(ls $OUTPUT_DIR | grep checkpoint-1[0-9][0-9][0-9]); do 
+    FINETUNE_WEIGHT=$OUTPUT_DIR/$FINETUNE_WEIGHT
+    echo ---$FINETUNE_WEIGHT---
+    echo ---$FINETUNE_WEIGHT--- >> $OUTPUT_DIR/eval.txt
+    python run_class_finetuning.py \
+    --model vit_base_patch16_224 \
+    --data_set UCF101 \
+    --nb_classes 5 \
+    --data_path ${DATA_PATH} \
+    --finetune ${FINETUNE_WEIGHT} \
+    --log_dir ${OUTPUT_DIR} \
+    --output_dir ${OUTPUT_DIR} \
+    --batch_size 8 \
+    --num_sample 1 \
+    --input_size 224 \
+    --short_side_size 224 \
+    --save_ckpt_freq 10 \
+    --num_frames 16 \
+    --sampling_rate 4 \
+    --opt adamw \
+    --lr 1e-3 \
+    --opt_betas 0.9 0.999 \
+    --weight_decay 0.05 \
+    --epochs 2000 \
+    --dist_eval \
+    --test_num_segment 5 \
+    --test_num_crop 3 \
+    --mixup 0 \
+    --cutmix 0 \
+    --smoothing 0 \
+    --eval >> $OUTPUT_DIR/eval.txt
+done
